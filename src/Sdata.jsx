2d3d29e25611603946bd195022ab86865dbcea01
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2020-09-23 02:12:54 +0500

    ading css and  array system

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        imgsrc:'https://i0.wp.com/kashmir.today/wp-content/uploads/2020/06/Dirilis-Ertugal_20200603-215356_1.jpg?fit=1065%2C1087&ssl=1',
+        title : 'A Netflix Orignal Series',
+        sname:'Ertugrul Ghazi',
+        link:'https://www.netflix.com/pk/title/80127001'
+    },
+    {
+        imgsrc:'https://nextalerts.com/wp-content/uploads/2020/05/thedarkposter-1024x576.jpg',
+        title : 'A Netflix Orignal Series',
+        sname:'DARK',
+        link:'https://www.netflix.com/pk/title/80100172'
+    },
+    {
+        imgsrc:'https://images-na.ssl-images-amazon.com/images/I/71OB1IywjLL._AC_SL1000_.jpg',
+       title : 'A Netflix Orignal Series',
+       sname:'Starnger Things',
+       link:'https://www.netflix.com/pk/title/80057281'
+    },
+    {
+        imgsrc:'https://images-na.ssl-images-amazon.com/images/I/61rwQngQEZL._AC_SL1278_.jpg',
+       title : 'A Netflix Orignal Series',
+       sname:'The Crown',
+       link:'https://www.netflix.com/pk/title/80025678'
+    },
+    {
+        imgsrc:'https://images-na.ssl-images-amazon.com/images/I/81SWlvJvkTL._AC_SL1500_.jpg',
+       title : 'A Netflix Orignal Series',
+       sname:'The Umbrella Academy',
+       link:'https://www.netflix.com/pk/title/80186863'
+    }
+]
+
+export default Sdata;
